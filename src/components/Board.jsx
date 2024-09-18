ecb4393371ac771fa601c90ea09714adf4587243
as-Solo <asdelsolo@gmail.com>
2024-09-18 17:47:27 +0200

    backup

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -9,28 +9,7 @@ function Board(props) {
       */}
       <div className="columna">Backlog <hr />
         <div className="columnaContenido">
-          <div className="ficha">
-            <div className="fichaHeader">
-              <div className="prioridad"></div>
-              <div className="Tarea">Design Landing Page</div>
-            </div>
-            <div className="descripcion">Create a visually appealing landing page for the website.</div>
-            <div className="asignado">
-              <p>assignee:</p>
-              <p>Mary Davis</p>
-            </div>
-            <div className="fichaFaldon">
-              <div className="fechaCreacion fechas">
-                <p className="detalle">create</p>
-                <p>2023-09-15</p>
-              </div>
-              <hr />
-              <div className="tiempoRestante fechas">
-                <p>8</p>
-                <p className="detalle">días restantes</p>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
       <div className="columna">To Do <hr /></div>
