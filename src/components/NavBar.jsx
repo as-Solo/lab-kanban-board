b7bd9571945295a4331e74a9c7c5eb6b1898fd6b
as-Solo <asdelsolo@gmail.com>
2024-09-23 13:00:28 +0200

    import de logos

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
+import logoPostIt from '../assets/prueba.png'
+import menuIcon from '../assets/menu.png'
+
 
 function NavBar(props) {
   return (
     <header id="navbar">
       <div className="bgLogos">
-        <img src="./src/assets/prueba.png" alt="logo app" className="logoKanbas"/>
+        <img src= {logoPostIt} alt="logo app" className="logoKanbas"/>
         <h1>Task<span>F</span>low</h1>
-        <img className="menu-btn" onClick={props.handle} src="./src/assets/menu.png" alt="menu icon"/>
+        <img className="menu-btn" onClick={props.handle} src={menuIcon} alt="menu icon"/>
       </div>
     </header>
   )
